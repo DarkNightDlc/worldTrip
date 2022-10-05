6a948ac8381d32e378b061e5f30f7365439b0a43
Pedro Henrique Ribeiro <pedronkdarck@gmail.com>
2022-10-04 21:53:51 -0300

    segurit env update

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,7 @@
 import * as prismic from '@prismicio/client'
 
-export const repositoryName = "https://spacetravelingdesafiodark.prismic.io/api/v2"
+export const repositoryName = process.env.NEXT_PUBLIC_PRISMIC_API_ENDPOINT
 
-export const client = prismic.createClient(repositoryName, {
+export const client = prismic.createClient(repositoryName!, {
     accessToken: process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN,
 })
